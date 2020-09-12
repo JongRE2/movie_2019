45d757584a7995e0e772500d313c2e3b929b4e37
kjy5947 <kjy5947@gmail.com>
2020-09-12 18:02:25 +0900

    movies api

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import PropTypes from "prop-types";
+import axios from "axios";
+
+class App extends React.Component{
+ 
+  state={
+  
+    isloading: true
+  };//state
+  
+  getMovies=async()=>{
+    const {data:{data:{movies}}} =await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    this.setState({movies,isloading:false});
+  };
+
+  componentDidMount(){
+    this.getMovies();
+  }
+
+  render(){
+    const {isloading}=this.state;     
+      return (
+      <div>
+      {this.state.isloading? "Loading...":"We are ready"}
+      </div>
+      );
+  }
+}
+
+export default App;
