7c6a093576c3d863e11a5ab4e67b64ea260cfdec
kjy5947 <kjy5947@gmail.com>
2020-09-12 18:06:31 +0900

    movies_API

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends React.Component{
   getMovies=async()=>{
     const {data:{data:{movies}}} =await axios.get("https://yts-proxy.now.sh/list_movies.json");
     this.setState({movies,isloading:false});
+    
   };
 
   componentDidMount(){
